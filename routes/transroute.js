18dda5f64723cf60bdc02085d4a1fb14e702be8e
zedar <zakrzewski.robert@gmail.com>
2013-07-09 20:35:26 +0200

    Added info about server context

--- routes/transroute.js
+++ routes/transroute.js
@@ -5,10 +5,14 @@ var common = require("./common.js");
 
 module.exports = {
   welcome: function(req, res) {
+    // get current server path
+    var url = req.protocol + "://" + req.headers.host + req.url;
     var lng = common.getCurrentLng(req);
     res.render("welcome", {
       title: "EuroSpedytor",
       lng: lng.i18n.t("menu.lang." + lng.lng),
-      usr: req.user ? req.user.username : null});
+      usr: req.user ? req.user.username : null,
+      url: url
+    });
   }
 }
