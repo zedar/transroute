00a7e694f3c52ebbbcb3f8ef17ffb833ae53cc02
zedar <zakrzewski.robert@gmail.com>
2013-08-18 12:05:59 +0200

    Added monitoring of process.env.PORT variable

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var io = require("socket.io").listen(server);
 
 // all environments
 //app.set("port", process.env.PORT || 3000);
-app.set("port", process.env.VMC_APP_PORT || 3000);
+app.set("port", process.env.VMC_APP_PORT || process.env.PORT || 3000);
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 app.use("/", express.static(path.join(__dirname, "public"))); // static resources has to be before session
